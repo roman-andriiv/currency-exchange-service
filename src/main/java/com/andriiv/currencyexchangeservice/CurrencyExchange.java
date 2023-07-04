047474a3e01d0dceb3a9317fecfa85e0a7fa389d
Roman Andriiv <roman.andriyiv@gmail.com>
2023-07-04 10:56:50 +0200

    [andriiv]  Setting up Dynamic Port in the Response

--- src/main/java/com/andriiv/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/andriiv/currencyexchangeservice/CurrencyExchange.java
@@ -10,11 +10,19 @@ import java.math.BigDecimal;
  * Created by Roman Andriiv (04.07.2023 - 10:34)
  */
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class CurrencyExchange {
     private Long id;
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
+    private String environment;
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
 }
+
